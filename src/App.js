c9d225b37edcdafb9231290cfe9217993ffd5eeb
MrinmoyBaust98 <baust.cse.170201073@gmail.com>
2023-02-08 23:00:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App center">
+     <Counter></Counter>
     </div>
   );
 }
+ 
+function Counter(){
+
+  // Custome style for button
+  const st={ backgroundColor:"peru", color:"white",marginLeft:"10px",padding:"5px",fontSize:"20px",borderRadius:"5px"}
+
+
+  // details bakkha note
+  const [count,setCount]=useState(0);
+  //increase Handler
+  const increaseHandle=()=>{
+    const newCount=count+1;
+    setCount(newCount);
+  }
+  //Decrease handler
+  const decreseHandle=()=>{
+    if(count>=1){
+      const newCount=count-1;
+      setCount(newCount);
+    }
+    else{
+      return;
+    }
+    
+  }
+  return(
+    <div className='counter-design'>
+      <h1>Count:{count}</h1>
+      <button style={st} onClick={increaseHandle}>Incrase</button>
+      <button style={st}onClick={decreseHandle}>Decrese</button>
+    </div>
+
+  );
+}
+
 
 export default App;
