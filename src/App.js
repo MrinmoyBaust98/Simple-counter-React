2d930687ec04abf98b659b667b349267a0b0144f
MrinmoyBaust98 <baust.cse.170201073@gmail.com>
2023-02-09 01:19:45 +0600

    Added Api Data load

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <div className="App center">
-     <Counter></Counter>
+    <div className="App">
+      <div className='center'>
+      <Counter></Counter>
+      </div>
+     <div>
+     <Externaluser> </Externaluser>
+     </div>
     </div>
   );
 }
@@ -23,6 +28,7 @@ function Counter(){
     const newCount=count+1;
     setCount(newCount);
   }
+
   //Decrease handler
   const decreseHandle=()=>{
     if(count>=1){
@@ -39,6 +45,47 @@ function Counter(){
       <h1>Count:{count}</h1>
       <button style={st} onClick={increaseHandle}>Incrase</button>
       <button style={st}onClick={decreseHandle}>Decrese</button>
+    </div>
+
+  );
+}
+
+
+// Data load Api
+
+function Externaluser(){
+
+  const [users,setUsers]= useState([]);
+    useEffect( ()=>{
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res=>res.json())
+      .then(data=>setUsers(data))
+    } ,[])
+
+
+  
+  return(
+    <div>
+      <h2>External user Using Api</h2>
+      <div className='center'>
+      <div className='counter-design'>
+      {
+        users.map(user=> <User Name={user.name} Email={user.email}></User>)
+      }
+      </div>
+      </div>
+      
+    </div>
+
+  );
+}
+
+function User(props){
+  return(
+    <div>
+      <h2> Name:  {props.Name}</h2>
+      <p> Email: {props.Email}</p>
+      
     </div>
 
   );
